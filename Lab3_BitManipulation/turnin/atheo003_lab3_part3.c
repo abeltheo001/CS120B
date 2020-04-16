1af20ec9592073f42f0ac2d7b35d181faf505ba1
Arsenal <atheo125@gmail.com>
2020-04-15 23:16:22 -0700

    Completed part 4

--- Lab3_BitManipulation/turnin/atheo003_lab3_part3.c
+++ Lab3_BitManipulation/turnin/atheo003_lab3_part3.c
@@ -1,7 +1,7 @@
 /*	Author: abeltheo
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #3  Exercise #2
+ *	Assignment: Lab #3  Exercise #3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
