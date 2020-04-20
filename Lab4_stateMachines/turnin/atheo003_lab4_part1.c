bc23414b3d6a7b6b4aa254f7d407cf1804f0fc60
Arsenal <atheo125@gmail.com>
2020-04-19 19:03:22 -0700

    completed part 3

---
+++ Lab4_stateMachines/turnin/atheo003_lab4_part1.c
@@ -0,0 +1,77 @@
+/*	Author: abeltheo
+ *  	Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #4  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+enum States {Start, Init, Press, Wait} state;
+#endif
+
+void Light() {
+
+	unsigned char A0 = PINA & 0x01;
+
+	switch(state){ //Transitions
+		case Start:
+			state = Init; 
+			break; 
+		case Init: 
+			if (A0 == 0x01)
+				state = Press;
+			else
+				state = Init;
+			break;
+		case Press: 
+			state = Wait;
+			break;
+		case Wait:
+			if (A0 == 0x01)
+				state = Init;
+			else 
+				state = Wait;
+			break;		
+		default: 
+			state = Start;
+			break;
+
+	}
+
+
+	switch(state){ //State
+		case Start:
+			PORTB = 0x01;
+			break;
+		case Init:
+			PORTB = 0x01;
+			break;
+		case Press:
+			PORTB = 0x02;
+			break;
+		case Wait:
+			PORTB = 0x02;
+		default:
+			PORTB = 0x01;
+		break; 
+
+
+	}
+}
+
+
+int main(void) {
+	
+	DDRA = 0x00; PORTA = 0xFF; 
+	DDRB = 0xFF; PORTB = 0x00;
+	
+	state = Start;
+	    
+	while (1) { Light();} 
+
+	return 1;
+}
--- Lab4_stateMachines/turnin/atheo003_lab4_part1_tests.gdb
+++ Lab4_stateMachines/turnin/atheo003_lab4_part1_tests.gdb
@@ -27,35 +27,44 @@ echo ======================================================\n
 echo Running all tests..."\n\n
 
 # Example test:
-# test "PINA: 0x00, PINB: 0x00 => PORTC: 0"
+test "PINA: 0x00, PINB: 0x00 => PORTC: 0"
 # Set inputs
-# setPINA 0x00
-# setPINB 0x00
+setPINA 0x00
+setPINB 0x00
 # Continue for several ticks
-# continue 2
+continue 2
 # Set expect values
-# expectPORTC 0
+expectPORTC 0
 # Check pass/fail
-# checkResult
+checkResult
 
 # Add tests below
 test "Test 1"
-set state = start
+set state = Start
+setPINA 0x01
+continue 2
 setPINA 0x00
-continue 3
-expectPORTC 0x07
-expect state wait
+continue 2
+expect state Wait
 checkResult
 
 test "Test 2" 
-set state = start
-setPINA 0x02
-continue 3
-expectPORTC 0x08
-continue 3
-expect state wait
+set state = Start
+setPINA 0x00
+continue 2 
+expect state Init
 checkResult
 
+test "Test 3" 
+set state = Start 
+setPINA 0x01 
+continue 2 
+setPINA 0x01
+continue 2 
+setPINA 0x01 
+continue 2 
+expect state Wait
+checkResult
 
 # Report on how many tests passed/tests ran
 set $passed=$tests-$failed
