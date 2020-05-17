6481b3dfadf463a1d35f31f9a0462b104cac105e
Abe Theo <atheo003@ucr.edu>
2020-05-17 16:48:26 -0700

    Completed parts 3 and 4

--- Lab8_ADC/turnin/atheo003_lab8_part4.c
+++ Lab8_ADC/turnin/atheo003_lab8_part4.c
@@ -29,7 +29,22 @@ int main(void) {
     /* Insert your solution below */
     while (1) {
 	unsigned short x = ADC;
-	if (x >= MAX/2)
+
+	if (x >= MAX)
+		PORTB = 0xFF;
+	else if (x >= 7*MAX/8)
+		PORTB = 0x7F;
+	else if (x >= 3*MAX/4)
+		PORTB = 0x3F;
+	else if (x >= 5*MAX/8)	
+		PORTB = 0x1F;
+	else if (x >= MAX/2)
+		PORTB = 0x0F;
+	else if (x >= 3*MAX/8)
+		PORTB = 0x07;
+	else if (x >= MAX/4)
+		PORTB = 0x03;
+	else if (x >= MAX/8)
 		PORTB = 0x01;
 	else
 		PORTB = 0x00;
