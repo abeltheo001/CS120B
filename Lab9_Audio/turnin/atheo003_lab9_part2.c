e46072476f319ae141a4114fe67d6d41b05c6434
Abe Theo <atheo003@ucr.edu>
2020-05-17 22:28:38 -0700

    Completed part 3

--- Lab9_Audio/turnin/atheo003_lab9_part2.c
+++ Lab9_Audio/turnin/atheo003_lab9_part2.c
@@ -1,7 +1,7 @@
 /*	Author: Abel Theodros
  * 	Partner(s) Name: n/a
  *	Lab Section: 024
- *	Assignment: Lab #9  Exercise #3
+ *	Assignment: Lab #9  Exercise #2
  *	Exercise Description: n/a
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -11,7 +11,6 @@
 #include <stdbool.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-#include "timer.h"
 #endif
 
 void set_PWM(double frequency) {
@@ -39,85 +38,95 @@ void PWM_off() {
 	TCCR3B = 0x00;
 }
 
-enum States{start, press, release, beat1, beat2, beat3, beat4, beat5}state;
+enum States{start, select, add, release, minus, on, offRelease, off}state;
 
 double note[8] = {261.63, 293.63,329.63,349.23,392,440,493.88,523.25}; 
 unsigned char count = 0; 
-int i = 0;
+
 void Tick(){
 	unsigned char button1 = ~PINA & 0x01;
-	unsigned char stop = ~PINA & 0x02;
+	unsigned char button2 = ~PINA & 0x02;
+	unsigned char button3 = ~PINA & 0x04;
+
 	switch(state) { //Transition
 	
 		case start: 
-			state = press; 
+			state = on; 
 			break;
 
-		case press: 
+		case select: 
 			if (button1)
-				state = release;
-			else if (stop)
-				state = press;
-			else
-				state = press;	
-			break;
-
-		case release:
-			if (button1)
-				state = release;
-			else if (stop)
-				state = press;
-			else
-				state = beat1;
-			break;
-		
-		case beat1:
-			if (stop)
-				state = press;
-			state = beat2; break;
-		case beat2:
-			if (stop) 
-				state = press;
-			state = beat3; break;
-		case beat3:
-			if (stop)
-				state = press;
-			state = beat4; break;
-		case beat4:
-			if (stop)
-				state = press;
-			state = beat5; break;
-		case beat5:
-			if (button1)
-				state = release;
-			else if (stop)
-				state = press;
+				state = add;
+			else if (button2)
+				state = minus; 
+			else if (button3)
+				state = off; 
 			else 
-				state = press;
+				state = select;
+			break;
+
+		case add:
+			state = release; break;
+		case release:
+			if (!button1 && !button2)
+				state = select;
+			else
+				state = release;
+			break;
+
+		case minus:	
+			state = release; break;
+		case off:
+			if (button3)
+				state = on;
+			else
+				state = off;
+			break;
+		case on:
+			if (button3)
+				state = on;
+			else
+				state = select;
+			break;
+	
 	 	default:
 			state = start; break; 
 	}
 
 	switch(state) { //State 
-		case start: break;
-		case press: set_PWM(0); break;
-		case release: break;
-	
-		case beat1:
-			set_PWM(note[6]);
-			break;	
-		case beat2:
-			set_PWM(note[1]);
+		case start: 
+			count = 0;	
 			break;
-		case beat3:
-			set_PWM(note[5]);
+		case select:		
+			 break;
+		case add:
+			if (count < 7)
+				count++;
+			set_PWM(note[count]);
 			break;
-		case beat4:
-			set_PWM(note[3]);
+
+		case release:
 			break;
-		case beat5:
-			set_PWM(note[4]);
+
+		case minus: 
+			if (count > 0)
+				count--;
+			set_PWM(note[count]);
 			break;
+
+		case offRelease: 
+			break;
+
+		case on:
+			PWM_on();
+			break;
+
+		case off:
+			PWM_off();
+			break;
+
+
+		
 			
 	} 
 }
@@ -126,14 +135,11 @@ int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
-	TimerSet(650);
-	TimerOn();
+
 	PWM_on();
     /* Insert your solution below */
 	while (1) {
 		Tick();
-		while (!TimerFlag);
-		TimerFlag = 0;
 	}
 	return 1;
 }
