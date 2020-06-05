0272c1565e980cc57e17150034f158be1b4d175c
Abe Theo <atheo003@ucr.edu>
2020-06-04 20:20:01 -0700

    merge update

--- Lab11_scheduler/source/io.c
+++ Lab11_scheduler/source/io.c
@@ -1,18 +1,11 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2512
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww10800\viewh14860\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
 
-\f0\fs24 \cf0 #include <avr/io.h>\
-#include <avr/interrupt.h>\
-#include <stdio.h>\
-#include "io.h"\
-\
-#define SET_BIT(p,i) ((p) |= (1 << (i)))\
-#define CLR_BIT(p,i) ((p) &= ~(1 << (i)))\
-#define GET_BIT(p,i) ((p) & (1 << (i)))\
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include "io.h"
+
+#define SET_BIT(p,i) ((p) |= (1 << (i)))
+#define CLR_BIT(p,i) ((p) &= ~(1 << (i)))
+#define GET_BIT(p,i) ((p) & (1 << (i))):
 \
 /*-------------------------------------------------------------------------*/\
 \
